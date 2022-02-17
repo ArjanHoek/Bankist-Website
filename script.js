637e4211b5c6754849f6fd47e7a7f87c8350b2cf
Arjan Hoek <arjanhoek5191@gmail.com>
2022-02-17 18:04:44 +0100

    add smooth scrolling and create page navigation with event delegation

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -27,5 +29,54 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+// Smooth scrolling
+
+/* Old method */
+// const smoothTo = el =>
+//   window.scrollTo({
+//     top: el.getBoundingClientRect().top + window.scrollY,
+//     behavior: 'smooth',
+//   });
+
+const smoothTo = el => el.scrollIntoView({ behavior: 'smooth' });
+
+btnScrollTo.addEventListener('click', function () {
+  smoothTo(section1);
+});
+
+// Page navigation
+// const navLinks = document.querySelectorAll('.nav__link');
+
+// navLinks.forEach(link =>
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const anchorId = link.getAttribute('href');
+
+//     if (anchorId === '#') {
+//       return;
+//     }
+
+//     const anchorEl = document.querySelector(anchorId);
+
+//     if (anchorEl) {
+//       smoothTo(anchorEl);
+//     }
+//   })
+// );
+
+const navLinks = document.querySelector('.nav__links');
+
+navLinks.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const anchorId = e.target.getAttribute('href');
+
+  if (anchorId && anchorId !== '#') {
+    const anchorEl = document.querySelector(anchorId);
+    smoothTo(anchorEl);
   }
 });
